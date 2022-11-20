39c2acee71e463acc27025ff1e8518d4c0d5c05a
majkeli122 <67538845+majkeli122@users.noreply.github.com>
2022-11-20 12:45:20 +0100

    added config

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests.exceptions
 import scratchclient
 import time
+import json
 
 print("Scratch F4F bot")
 print("-------------------------------------")
 print("This is just some sucky code which searches for Scratch users in your following tab, and checks the users they follow as well.")
 print("If you don't follow any new people, follow someone else manually.")
 print("I don't take responsiblity if you get banned for using this.")
+
+config = open("config.json", "r")
+config_loaded = json.loads(config.read())
 
 username = input("Enter Scratch account username: ")
 password = input("Enter Scratch account password: ")
@@ -24,5 +28,6 @@ while True:
                     if not g.username == username:
                         print(g.username)
                         g.follow()
+                        time.sleep(config_loaded["timeInterval"])
         except requests.exceptions.JSONDecodeError:
             pass
